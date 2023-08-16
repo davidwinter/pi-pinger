8b3953ee525af4a5dd18e495703c279fca5a4165
David Winter <i@djw.me>
2023-08-16 18:14:27 +0100

    fix: remove delimeter option as not needed

--- crontab.sh
+++ crontab.sh
@@ -3,4 +3,4 @@
 date
 echo "Running heartbeat pings..."
 echo $PUSH_TESTS
-echo $PUSH_TESTS | xargs -d ' ' -n 1 -P 5 -I '{}' curl --silent "{}"
+echo $PUSH_TESTS | xargs -n 1 -P 5 -I '{}' curl --silent "{}"
