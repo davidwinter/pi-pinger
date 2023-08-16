bd111434f51cac8dcef9e60686b718bb0ecec596
David Winter <i@djw.me>
2023-08-16 19:33:50 +0100

    refactor: remove extra logs that are redundant

--- crontab.sh
+++ crontab.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 
-date
-echo "Running heartbeat pings..."
 echo "${MULTIPLE_PUSH_TESTS_BASE64}" | base64 -d | xargs -I '{}' echo "{}"
 echo "${MULTIPLE_PUSH_TESTS_BASE64}" | base64 -d | xargs -I '{}' -P 5 curl --silent "{}"
