0a8cacf9fcfdf90773ef76553fe87ded9ef8189d
David Winter <i@djw.me>
2023-08-16 19:29:51 +0100

    fix: use base64 decode to fix line break in secrets issue

--- crontab.sh
+++ crontab.sh
@@ -2,5 +2,5 @@
 
 date
 echo "Running heartbeat pings..."
-echo "${PUSH_TESTS}" | xargs --delimiter=' ' -I '{}' echo "{}"
-echo "${PUSH_TESTS}" | xargs --delimiter=' ' -I '{}' -P 5 curl --silent "{}"
+echo "${MULTIPLE_PUSH_TESTS_BASE64}" | base64 -d | xargs -I '{}' echo "{}"
+echo "${MULTIPLE_PUSH_TESTS_BASE64}" | base64 -d | xargs -I '{}' -P 5 curl --silent "{}"
