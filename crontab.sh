0c6bc729e8d516388f9789c68591ce2b6a649e22
David Winter <i@djw.me>
2023-08-20 07:31:43 +0100

    refactor: use PING_URLs as environment variable for readability

--- crontab.sh
+++ crontab.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-echo "${MULTIPLE_PUSH_TESTS_BASE64}" | base64 -d | xargs -I '{}' echo "{}"
-echo "${MULTIPLE_PUSH_TESTS_BASE64}" | base64 -d | xargs -I '{}' -P 5 curl --silent "{}"
+echo "${PING_URLS}" | xargs -I '{}' echo "{}"
+echo "${PING_URLS}" | xargs -I '{}' -P 5 curl --silent "{}"
